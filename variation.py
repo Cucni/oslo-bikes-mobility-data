3ab8ae135f3ff2f7a5545017db03e5378ebb65c5
Cucno <5228299-cucno@users.noreply.gitlab.com>
2020-08-21 15:22:00 +0200

    Add alongside plot of 5-day average rides in years

--- variation.py
+++ variation.py
@@ -22,8 +22,8 @@ df_20 = pd.concat(datasets20)
 daily_2019 = df_19.groupby(df_19['started_at'].dt.date).count()['started_at']
 daily_2020 = df_20.groupby(df_20['started_at'].dt.date).count()['started_at']
 
-daily_2019.name = 'Rides'
-daily_2020.name = 'Rides'
+daily_2019.name = 'Rides 2019'
+daily_2020.name = 'Rides 2020'
 daily_2019.index.name = 'Day'
 daily_2020.index.name = 'Day'
 
@@ -33,8 +33,18 @@ rolling_2019.index = rolling_2019.index.astype("datetime64[ns]").dayofyear
 rolling_2020.index = rolling_2020.index.astype("datetime64[ns]").dayofyear
 
 
-variation = (rolling_2020/rolling_2019) - 1
+variation = ((rolling_2020/rolling_2019) - 1)*100
+rolling = pd.concat([rolling_2019,rolling_2020],axis=1)
 
-fig = plt.figure()
+rolling.plot(color=['tab:blue','tab:orange'])
+plt.title("5-day Rides rolling average in 2019 and 2020")
+plt.xlabel("Day of the year")
+plt.ylabel("Number of rides")
+plt.savefig('total_rides.png')
+
+plt.figure()
 variation.plot()
-fig.savefig('variation.png')
+plt.title("Relative variation in the number of rides from 2019 to 2020")
+plt.xlabel("Day of the year")
+plt.ylabel("Percentage variation")
+plt.savefig('variation.png')
