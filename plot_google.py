6201fb1708148657869240eaf2f50a012d794e0d
Cucno <5228299-cucno@users.noreply.gitlab.com>
2020-08-28 15:28:49 +0200

    Write script loading+plotting Google data for Oslo
    I have written a simple script that loads Google's mobility data in a
    pandas dataframe, and the plots the variation in the transit stations
    mobility relative to the baseline. The plot is saved in a file. This is
    needed for future comparison with the bike mobility data and variation.

---
+++ plot_google.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+#This script loads Google data on mobility in Oslo, and then plots the variation as computed by Google. We are interested in the "transportation" column of the Google dataset.
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+plt.style.use('seaborn')
+
+df = pd.read_csv("GOG-Global_Mobility_Report.csv",parse_dates=[7])
+#df_month = df[df["date"].dt.month.isin([5,6,7])].copy()
+sns.lineplot(data=df[df["date"].dt.month.isin([5,6,7])],x="date",y="transit_stations_percent_change_from_baseline")
+plt.title("Google's relative variation in transit stations mobility")
+plt.ylabel("Transit stations percent variation from baseline")
+plt.xticks(rotation=45)
+plt.savefig("google_variation.pdf")
