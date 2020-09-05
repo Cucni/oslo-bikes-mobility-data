82d7f96c0681d61d878e056d7e051655f4cd725d
Cucno <5228299-cucno@users.noreply.gitlab.com>
2020-09-06 00:03:10 +0200

    Make plot_google into a function
    I have defined the function plot_google() which takes care of plotting
    Google's data. I have also used the usual conditional for __main__ to
    avoid extra plotting when importing as a module.

--- plot_google.py
+++ plot_google.py
@@ -8,10 +8,14 @@ import matplotlib.pyplot as plt
 
 plt.style.use('seaborn')
 
-df = pd.read_csv("GOG-Global_Mobility_Report.csv",parse_dates=[7])
-#df_month = df[df["date"].dt.month.isin([5,6,7])].copy()
-sns.lineplot(data=df[df["date"].dt.month.isin([5,6,7,8])],x="date",y="transit_stations_percent_change_from_baseline")
-plt.title("Google's relative variation in transit stations mobility")
-plt.ylabel("Transit stations percent variation from baseline")
-plt.xticks(rotation=45)
-plt.savefig("google_variation.pdf")
+def plot_google():
+    df = pd.read_csv("GOG-Global_Mobility_Report.csv",parse_dates=[7])
+    #df_month = df[df["date"].dt.month.isin([5,6,7])].copy()
+    sns.lineplot(data=df[df["date"].dt.month.isin([5,6,7,8])],x="date",y="transit_stations_percent_change_from_baseline")
+    plt.title("Google's relative variation in transit stations mobility")
+    plt.ylabel("Transit stations percent variation from baseline")
+    plt.xticks(rotation=45)
+    plt.savefig("google_variation.pdf")
+
+if __name__=='__main__':
+    plot_google()
