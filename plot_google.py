9b0ccbca1d4052e4f75ee2ca1215344a7c10c327
Cucno <5228299-cucno@users.noreply.gitlab.com>
2020-09-07 00:37:38 +0200

    Use dayofyear in plotting Google's data
    In this way I have the same x axis as in the variation output, and they
    are better comparable.

--- plot_google.py
+++ plot_google.py
@@ -10,8 +10,9 @@ plt.style.use('seaborn')
 
 def plot_google():
     df = pd.read_csv("GOG-Global_Mobility_Report.csv",parse_dates=[7])
+    df["doy"] = df["date"].dt.dayofyear
     #df_month = df[df["date"].dt.month.isin([5,6,7])].copy()
-    sns.lineplot(data=df[df["date"].dt.month.isin([5,6,7,8])],x="date",y="transit_stations_percent_change_from_baseline")
+    sns.lineplot(data=df[df["date"].dt.month.isin([5,6,7,8])],x="doy",y="transit_stations_percent_change_from_baseline")
     plt.title("Google's relative variation in transit stations mobility")
     plt.ylabel("Transit stations percent variation from baseline")
     plt.xticks(rotation=45)
