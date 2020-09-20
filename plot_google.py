00ec78f55e2eb0ad9e3ff302e9f5a0352d17687c
Cucno <5228299-cucno@users.noreply.gitlab.com>
2020-09-20 20:10:16 +0200

    Split loading and plotting Google's dataset

--- plot_google.py
+++ plot_google.py
@@ -8,10 +8,13 @@ import matplotlib.pyplot as plt
 
 plt.style.use('seaborn')
 
-def plot_google():
+def load_google():
     df = pd.read_csv("GOG-Global_Mobility_Report.csv",parse_dates=[7])
     df["doy"] = df["date"].dt.dayofyear
-    #df_month = df[df["date"].dt.month.isin([5,6,7])].copy()
+    return df
+
+def plot_google():
+    df = load_google()
     sns.lineplot(data=df[df["date"].dt.month.isin([5,6,7,8])],x="doy",y="transit_stations_percent_change_from_baseline")
     plt.title("Google's relative variation in transit stations mobility")
     plt.ylabel("Transit stations percent variation from baseline")
